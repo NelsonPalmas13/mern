67d6a74d2f45643a4c95ab9b5253c6e4a76c42b6
Nelson13 <1150829@isep.ipp.pt>
2020-08-03 14:38:33 +0100

    Continuing frontend (Beginning mobile view)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users'
 import NewRecipe from './recipes/pages/NewRecipe';
+import MainNavigation from './shared/components/UIElements/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/recipes/new" exact>
-          <NewRecipe />
-        </Route>
-        <Redirect to="/"/>
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/recipes/new" exact>
+            <NewRecipe />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
